405edc2b9534cb9379bef7aab5dbe1315c443868
Asfakur <saikat.hap@gmail.com>
2021-03-09 14:53:53 +0600

    38-6 Please Order, Clear state, show conditional image.

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, processOrder, removeFromDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import happyImage from '../../images/giphy.gif';
 
 const Review = () => {
 
     // const {cart, setCart} = useState([]); this will be array
     const [cart, setCart] = useState([]);
+    const [orderPlaced, setOrderPlaced] = useState(false);
+
+    const handlePlaceOrder = () => {
+        setCart([]);
+
+        setOrderPlaced(true);
+
+        processOrder();
+    }
 
     const removeProduct = (productKey) => {
         const newCart = cart.filter(pd => pd.key !== productKey);
@@ -27,7 +37,13 @@ const Review = () => {
             
         });
         setCart(cartProducts);
-    }, [])
+    }, []);
+
+    let thankyou; 
+    if(orderPlaced){
+        thankyou = <img src={happyImage} alt=""/>;
+    }
+
     return (
         <div className="twin-container">
             <div className="product-container">
@@ -39,9 +55,16 @@ const Review = () => {
                         product={pd}
                         ></ReviewItem>)
                 }
+
+                {
+                    thankyou
+                }
+
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart}>
+                    <button onClick={handlePlaceOrder} className="main-button">Place Order</button>
+                </Cart>
             </div>
             
         </div>
